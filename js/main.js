f594463a02ec4c4298b466870e3d912c0cbbee04
octo <octoground@yandex.ru>
2023-06-30 19:52:22 +0500

    тест мобильной версии

--- js/main.js
+++ js/main.js
@@ -68,12 +68,12 @@ $(document).on('blur', 'form .input', function (e) {
  */
 
 window.addEventListener('resize', resize);
-window.addEventListener('load', resize);
+// window.addEventListener('load', resize);
 
-function resize() {
-    let vh = document.querySelector('.main');
-    vh.innerHTML = vh.offsetHeight;
+// function resize() {
+//     let vh = document.querySelector('.main');
+//     vh.innerHTML = vh.offsetHeight;
 
-    let perc = document.querySelector('.service');
-    perc.innerHTML = perc.offsetHeight
-}
+//     let perc = document.querySelector('.service');
+//     perc.innerHTML = perc.offsetHeight
+// }
